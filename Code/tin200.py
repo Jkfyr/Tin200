f524d4ac0b3808610a1b17e4a301d833ca8c3134
Jens Krisitan <Jholmboe@nmbu.no>
2021-06-03 13:40:10 +0200

    side and slider update (stremlit)

--- Code/tin200.py
+++ Code/tin200.py
@@ -30,17 +30,17 @@ print(float(olo.CoapplicantIncome.mean()))
 
 def user_value():
     #Loan_ID =
-    Gender_Imputed = st.sidebar.slider('Gender', float(olo.Gender_Imputed.min()), float(olo.Gender_Imputed.max()), float(olo.Gender_Imputed.mean()))
+    Gender_Imputed = st.sidebar.slider('Gender', 0, 1)
     Married_Imputed = st.sidebar.slider('Married', 0, 1)
-    Dependents_Imputed = st.sidebar.slider('Dependents', float(olo.Dependents_Imputed.min()), float(olo.Dependents_Imputed.max()), float(olo.Dependents_Imputed.mean()))
-    Education_Imputed = st.sidebar.slider('Education', float(olo.Education_Imputed.min()), float(olo.Education_Imputed.max()), float(olo.Education_Imputed.mean()))
-    #Self_Employed_Imputed = st.sidebar.slider('Self_Employed', float(olo.Self_Employed_Imputed.min()), float(olo.Self_Employed.max()), float(olo.Self_Employed_Imputed.mean()))
+    Dependents_Imputed = st.sidebar.slider('Dependents', 0, 1, 3)
+    Education_Imputed = st.sidebar.slider('Education', 0, 1)
+    #Self_Employed_Imputed = st.sidebar.slider('Self_Employed', 0, 1)
     ApplicantIncome = st.sidebar.slider('ApplicantIncome', float(olo.ApplicantIncome.min()), float(olo.ApplicantIncome.max()), float(olo.ApplicantIncome.mean()))
     CoapplicantIncome= st.sidebar.slider('CoapplicantIncome', float(olo.CoapplicantIncome.min()), float(olo.CoapplicantIncome.max()), float(olo.CoapplicantIncome.mean()))
     LoanAmount= st.sidebar.slider('LoanAmount', float(olo.LoanAmount.min()), float(olo.LoanAmount.max()), float(olo.LoanAmount.mean()))
     Loan_Amount_Term = st.sidebar.slider('Loan_Amount_Term ', float(olo.Loan_Amount_Term .min()), float(olo.Loan_Amount_Term .max()), float(olo.Loan_Amount_Term .mean()))
     Credit_History = st.sidebar.slider('Credit_History', float(olo.Credit_History.min()), float(olo.Credit_History.max()), float(olo.Credit_History.mean()))
-    Property_Area_Imputed = st.sidebar.slider('Property_Area', float(olo.Property_Area_Imputed.min()), float(olo.Property_Area_Imputed.max()), float(olo.Property_Area_Imputed.mean()))
+    Property_Area_Imputed = st.sidebar.slider('Property_Area', 0, 1, 2)
     #Loan_Status = st.sidebar('Loan_Status', float(olo.Loan_Status.min()), float(olo.Loan_Status.max()), float(olo.Loan_Status.mean()))
 
     data = { #'Loan_ID' : 0,
